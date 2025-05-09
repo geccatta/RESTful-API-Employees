d3fc0d404a99b0a039b6e09aeacf3cb8ffda6a7b
Genadi Iliev <genito771@gmail.com>
2025-05-09 12:09:15 +0300

    Delete crm-rest-api-employees directory

--- crm-rest-api-employees/spring-boot-rest-crud-employee/src/main/java/com/geccatta/crud_demo/errorResponse/exceptions/EmployeeDoesntExist.java
+++
@@ -1,7 +0,0 @@
-package com.geccatta.crud_demo.errorResponse.exceptions;
-
-public class EmployeeDoesntExist extends RuntimeException{
-    public EmployeeDoesntExist(String message) {
-        super(message);
-    }
-}
--- crm-rest-api-employees/spring-boot-rest-crud-employee/src/main/java/com/geccatta/crud_demo/errorResponse/exceptions/EmployeeNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.geccatta.crud_demo.errorResponse.exceptions;
-
-public class EmployeeNotFound extends RuntimeException{
-    public EmployeeNotFound(String message) {
-        super(message);
-    }
-}
