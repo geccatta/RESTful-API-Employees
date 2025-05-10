f83c91ff275b7380bee42133d75b7048866cea1d
geccatta <genito771@gmail.com>
2025-05-10 21:54:57 +0300

    Remove unnecessary folders: __MACOSX and crm-rest-api-employees

--- crm-rest-api-employees/spring-boot-rest-crud-employee/src/main/java/com/geccatta/crud_demo/errorResponse/exceptions/EmployeeIDModificationException.java
+++
@@ -1,7 +0,0 @@
-package com.geccatta.crud_demo.errorResponse.exceptions;
-
-public class EmployeeIDModificationException extends RuntimeException{
-    public EmployeeIDModificationException(String message) {
-        super(message);
-    }
-}
